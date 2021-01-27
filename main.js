2be4b9773aaba50ca2d9dc8a1e03a81dc6030f78
wypdik <72793678+wypdik@users.noreply.github.com>
2021-01-27 21:57:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -20,10 +20,10 @@ btn.onclick = function(){
   if(btnclass === "dark"){
     btn.setAttribute('class', "light");
     btn.textContent = "Lighten";
-    overlay.style.backgroundColor = rgba(0,0,0,0.5);
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
   }else{
     btn.setAttribute('class', "dark");
     btn.textContent = "Darken";
-    overlay.style.backgroundColor = rgba(0,0,0,0);
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
   }
 }
